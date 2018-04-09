5ed532adb21ad27f7c3910dd519f8753992db5e9
Agafonov Ivan <sudden1306@gmail.com>
2018-04-09 23:11:54 +0300

    start)

---
+++ lab_2/lab_2.py
@@ -0,0 +1,6 @@
+from tkinter import *
+# Основная сцена
+root=Tk()
+root.title('Лабораторная работа №1')
+root.geometry('1280x768+400+10') # ширина=1024, высота=768, смещение x=600, смещение y=200
+root.mainloop()
